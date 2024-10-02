70013fa4bafd5b5131567eeb34a481517b8f8793
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-02 12:26:44 +0600

    how many datatype in javascript

---
+++ datatype.js
@@ -0,0 +1,21 @@
+primitive
+
+Number = 1, 2, 3, 4, 5
+
+String = 'this is a string data'
+
+Boolean = true // false
+
+Object = {
+    ary1: [1, 3, 4],
+    ary2: ['number', 8, 9, 25, 'mixedDataArray'],
+    sum: a+b,
+    devided: 55/5
+}
+
+Array = [1, 2, 3, 4, 5, 6]
+
+function sum(a, b){
+    return a + b
+}
+console.log(sum(12, 12));
