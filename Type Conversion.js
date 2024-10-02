5ee0964d2bdbc58227fc2350dd80f437e0175825
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-02 16:38:06 +0600

    Type Conversion in Javascript

---
+++ Type Conversion.js
@@ -0,0 +1,14 @@
+let str = '1000'
+
+let num = 50
+
+console.log(str + num);
+console.log(Number(str) + num);
+console.log(Number.parseInt(str) + num);
+console.log(Number.parseInt(str) + num.toString());
+
+let bool = 1
+console.log(bool);
+let x =console.log(Boolean(bool));
+ 
+
