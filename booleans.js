16e53b5ff9349561f91e941c9466dff14fc5ed1c
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-02 12:58:31 +0600

    how to work boolean literal and constructor in Javascript

---
+++ booleans.js
@@ -0,0 +1,15 @@
+//Boolean literal
+
+let b1 = true
+console.log(b1);
+
+let b2 = false
+console.log(b2);
+
+//Boolean Constructor
+
+let b3 = Boolean(true)
+console.log(b3);
+
+let b4 = Boolean(false)
+console.log(b4);
