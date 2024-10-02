0c3b7f53f0248ffac5fdbbdea76edc0b10cfe51d
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-02 12:47:56 +0600

    how to work string literal and constructor in Javascript

---
+++ string.js
@@ -0,0 +1,12 @@
+let str1 = "this is a string"
+console.log(str1);
+
+let str2 = 'this is an other string'
+console.log(str2);
+
+let str3 =`this is a back-tick string`
+console.log(str3);
+
+let str4 = String("this is Constructor string" + 8273)
+console.log(str4);
+
