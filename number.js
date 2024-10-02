886af9eb7efa03e3355271c139ecc3b4528ce50b
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-02 12:37:50 +0600

    number type in Javascript

---
+++ number.js
@@ -0,0 +1,11 @@
+let intnum = 10
+console.log(intnum);
+
+let floatnum = 35.43
+console.log(floatnum);
+
+let infinity = 10/2
+console.log(infinity);
+
+let nannum = 'jock' * 3 //not a number
+console.log(nannum);
