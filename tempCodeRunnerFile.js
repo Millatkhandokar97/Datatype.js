02bbe1da7bd9b16dd9cc7bcad1f6f8fd8073fb0c
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-03 17:36:51 +0600

    Create tempCodeRunnerFile.js

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+else if(a1 > a2 || a3 < a4){
+//     console.log('A1 is greater then A2 and A4 is greater then A3');
+// }
+// else if(a1 < a2 || a3 < a4){
+//     console.log('A2 is greater then A1 and A4 is greater then A3');
+// }
